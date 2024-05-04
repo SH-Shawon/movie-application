473bba0fd8c15fa26756e18a6269598c8f248b10
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 14:37:23 +0600

    adding movie to movie application is done. test case passed

--- src/main/java/com/sakibul/movie_application/application/MovieApplication.java
+++ src/main/java/com/sakibul/movie_application/application/MovieApplication.java
@@ -1,15 +1,19 @@
 package com.sakibul.movie_application.application;
 
+import com.sakibul.movie_application.entities.Movie;
 import com.sakibul.movie_application.entities.User;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieApplication {
     private List<User> users;
+    private List<Movie> allMovies;
 
     public MovieApplication() {
         users = new ArrayList<>();
+        allMovies = new ArrayList<>();
     }
 
     public List<User> getUsers() {
@@ -20,6 +24,13 @@ public class MovieApplication {
         this.users.add(user);
     }
 
+    public List<Movie> getAllMovies() {
+        return allMovies;
+    }
+
+    public void setAllMovies(List<Movie> allMovies) {
+        this.allMovies = allMovies;
+    }
     public void registerUser(String email) {
         users.add(new User(email));
     }
@@ -31,6 +42,10 @@ public class MovieApplication {
             }
         }
         return null;
+    }
+
+    public void addMovie(String title, List<String> cast, List<String> category, LocalDate releaseDate, int budget) {
+        allMovies.add(new Movie(title, cast, category, releaseDate, budget));
     }
 
 }
