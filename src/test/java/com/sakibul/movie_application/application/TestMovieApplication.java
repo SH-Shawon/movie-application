f8856f3bf254fc1c02354c4ea1d2894250514515
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 14:32:18 +0600

    test written for addMovie to movie application is done. test case is failed as expected.

--- src/test/java/com/sakibul/movie_application/application/TestMovieApplication.java
+++ src/test/java/com/sakibul/movie_application/application/TestMovieApplication.java
@@ -1,7 +1,12 @@
 package com.sakibul.movie_application.application;
 
+import com.sakibul.movie_application.entities.Movie;
 import com.sakibul.movie_application.entities.User;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,5 +20,28 @@ public class TestMovieApplication {
         User user = movieApplication.getUserDetails(email);
         assertNotNull(user);
         assertEquals(email,user.getEmail());
+    }
+
+    @Test
+    public void testAddMovie() {
+        MovieApplication movieApplication = new MovieApplication();
+        String title = "The Shawshank Redemption";
+        List<String> cast = new ArrayList<>();
+        cast.add("Tim Robbins");
+        cast.add("Morgan Freeman");
+        List<String> category = List.of("Drama");
+        LocalDate releaseDate = LocalDate.parse("1994-10-14");
+        int budget = 25000000;
+
+        movieApplication.addMovie(title, cast, category, releaseDate, budget);
+
+        List<Movie> allMovies = movieApplication.getAllMovies();
+        assertEquals(1, allMovies.size());
+        Movie movie = allMovies.get(0);
+        assertEquals(title, movie.getTitle());
+        assertEquals(cast, movie.getCast());
+        assertEquals(category, movie.getCategory());
+        assertEquals(releaseDate, movie.getReleaseDate());
+        assertEquals(budget, movie.getBudget());
     }
 }
