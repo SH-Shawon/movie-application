eccdc67533b66d93f05cfab56cd4333973b0fb9f
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 12:53:21 +0600

    addToFavourites is successful

---
+++ src/main/java/com/sakibul/movie_application/entitys/Movie.java
@@ -0,0 +1,61 @@
+package com.sakibul.movie_application.entitys;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private List<String> cast;
+    private List<String> category;
+    private LocalDate releaseDate;
+    private int budget;
+
+    public Movie(String title, List<String> cast, List<String> category, LocalDate releaseDate, int budget) {
+        this.title = title;
+        this.cast = cast;
+        this.category = category;
+        this.releaseDate = releaseDate;
+        this.budget = budget;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getCast() {
+        return cast;
+    }
+
+    public void setCast(List<String> cast) {
+        this.cast = cast;
+    }
+
+    public List<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<String> category) {
+        this.category = category;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+}
--- src/main/java/com/sakibul/movie_application/entitys/User.java
+++ src/main/java/com/sakibul/movie_application/entitys/User.java
@@ -1,10 +1,15 @@
 package com.sakibul.movie_application.entitys;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String email;
+    private List<Movie> favourites;
 
     public User(String email) {
         this.email = email;
+        favourites = new ArrayList<>();
     }
 
     public String getEmail() {
@@ -13,6 +18,14 @@ public class User {
 
     public void setEmail(String mail) {
         this.email = mail;
+    }
+
+    public List<Movie> getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(Movie movie) {
+        favourites.add(movie);
     }
 
 }
