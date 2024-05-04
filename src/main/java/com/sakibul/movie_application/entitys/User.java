63a9d78afcf9f311fae3832d5ad232fcba91b50b
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 12:32:26 +0600

    creating user done successfully

---
+++ src/main/java/com/sakibul/movie_application/entitys/User.java
@@ -0,0 +1,18 @@
+package com.sakibul.movie_application.entitys;
+
+public class User {
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String mail) {
+        this.email = mail;
+    }
+
+}
