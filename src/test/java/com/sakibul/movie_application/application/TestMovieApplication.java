c6be271a541916f5b3fc815e10c399aae468cb58
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 13:27:41 +0600

    test written for registeringUser in movie application is done. but test is failed till now

---
+++ src/test/java/com/sakibul/movie_application/application/TestMovieApplication.java
@@ -0,0 +1,18 @@
+package com.sakibul.movie_application.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestMovieApplication {
+    @Test
+    public void testRegisteringUser(){
+        MovieApplication movieApplication = new MovieApplication();
+        String email = "shawon@gmail.com";
+        movieApplication.registerUser(email);
+        User user = movieApplication.getUserDetails(email);
+        assertNotNull(user);
+        assertEquals(email,user.getEmail());
+    }
+}
