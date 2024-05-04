2c5eebeefa8021027e44eab0d42d96db4b6cf486
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 12:01:24 +0600

    First commit after creation spring boot project

---
+++ src/test/java/com/sakibul/movie_application/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakibul.movie_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
