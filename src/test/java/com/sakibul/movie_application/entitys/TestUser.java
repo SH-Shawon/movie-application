abd712bcaeaf064281cd2d4704f504c1b0d0f73f
Sakibul_Hasan <shshaon39@gmail.com>
2024-05-04 12:35:40 +0600

    creat test for addToFavourites. But this test is failed now

--- src/test/java/com/sakibul/movie_application/entitys/TestUser.java
+++ src/test/java/com/sakibul/movie_application/entitys/TestUser.java
@@ -2,12 +2,36 @@ package com.sakibul.movie_application.entitys;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestUser {
     @Test
     public void testCreateUser() {
         User user = new User("sakibul@gmail.com");
         assertEquals("sakibul@gmail.com", user.getEmail());
+    }
+
+    @Test
+    public void testAddToFavorites() {
+        User user = new User("sakibul@gmail.com");
+
+        String title = "The Shawshank Redemption";
+        List<String> cast = new ArrayList<>();
+        cast.add("Tim Robbins");
+        cast.add("Morgan Freeman");
+        List<String> category = List.of("Drama");
+        LocalDate releaseDate = LocalDate.parse("1994-10-14");
+        int budget = 25000000;
+        Movie movie = new Movie(title, cast, category, releaseDate, budget);
+
+        user.setFavourites(movie);
+
+        assertEquals(1, user.getFavourites().size());
+        assertTrue(user.getFavourites().contains(movie));
     }
 }
